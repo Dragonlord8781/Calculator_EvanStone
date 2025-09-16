642f8602af31cafb5d3978d7bab516777d3a3511
Dragonlord8781 <evan.stone638@gmail.com>
2025-09-16 08:06:21 -0500

    Initial commit

---
+++ Calculator_EvanStone/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator_EvanStone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
